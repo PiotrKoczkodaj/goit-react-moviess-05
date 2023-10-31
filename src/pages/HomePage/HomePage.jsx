27a6d4bebf23f83748cd82b76cd5cb4884896651
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-10-31 17:20:41 +0100

    .

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,8 @@
-import { api } from "api"
-import { useState } from "react"
+// import { api } from "api"
+// import { useState } from "react"
 export const HomePage = () => {
     
-    const [movie, setMovie] = useState([]);
+    // const [movie, setMovie] = useState([]);
     
     return (
         <div>
