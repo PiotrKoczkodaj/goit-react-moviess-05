9c51471ea7a69a984ccb36b42052ec3849b7716d
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-11-02 13:44:28 +0100

    .

--- src/pages/HomePage/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage/HomePage.jsx
@@ -17,7 +17,7 @@ export const HomePage = () => {
       <h2>Trending today</h2>
           <ul>
               {movies.map(movie => (
-                  <li  key={movie.id}><Link to={`movies/${movie.id}`}>{movie.id }</Link></li>
+                  <li  key={movie.id}><Link to={`movies/${movie.id}`}>{movie.title|| movie.name}</Link></li>
               ))}
       </ul>
     </div>
